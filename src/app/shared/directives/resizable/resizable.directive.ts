05543a1d6870b5746cec04c0381c8a87969e651d
Marat Ibragimov <Marat.Ibragimov@infowatch.com>
2023-10-29 15:42:52 +0300

    add cursor

--- src/app/shared/directives/resizable/resizable.directive.ts
+++ src/app/shared/directives/resizable/resizable.directive.ts
@@ -1,11 +1,32 @@
-import { Directive, ElementRef, HostBinding } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  ContentChild,
+  Directive,
+  ElementRef,
+  HostBinding,
+  Inject,
+  OnInit,
+} from '@angular/core';
 
 @Directive({
   selector: '[appResizable]',
 })
-export class ResizableDirective {
+export class ResizableDirective implements OnInit {
+  @ContentChild('resizeHandler', { static: true })
+  private _resizeHandler?: ElementRef;
+
   @HostBinding('style.width')
   private _width?: string;
 
-  constructor(private readonly _el: ElementRef) {}
+  constructor(
+    @Inject(DOCUMENT)
+    private readonly _document: Document,
+    private readonly _el: ElementRef
+  ) {}
+
+  ngOnInit(): void {
+    if (this._resizeHandler) {
+      this._resizeHandler.nativeElement.style.cursor = 'col-resize';
+    }
+  }
 }
